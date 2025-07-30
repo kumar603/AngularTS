5e382f5d929f8c46f794639a21c9403f5160e456
Kiran Kumar <kumar603>
2025-07-30 18:15:30 +0530

    Angular TS -Services-Routing And Navigations

--- student-course-app/src/app/app.component.ts
+++ student-course-app/src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>My Angular App</h1>
+ imports: [RouterOutlet],
+  // imports: [RouterModule],
+  // template: `
+  //   <h1>My Angular App</h1>
+  //   <nav>
+  //     <a routerLink="/databinding">Data Binding</a> |
+  //     <a routerLink="/directives">Directives</a>
+  //   </nav>
+  //   <router-outlet></router-outlet> <!-- This is where your routed component will show -->
+  // `
+   template: `
     <nav>
-      <a routerLink="/databinding">Data Binding</a> |
-      <a routerLink="/directives">Directives</a>
+      <a routerLink="/">Home</a> |
+      <a routerLink="/about">About</a> |
+      <a routerLink="/students">Students</a>
     </nav>
-    <router-outlet></router-outlet> <!-- This is where your routed component will show -->
-  `
+    <hr />
+    <router-outlet></router-outlet>`
 })
 export class AppComponent {}
--- student-course-app/src/app/app.routes.ts
+++ student-course-app/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 import { DataBindingComponent } from './databinding/databinding.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+ 
 
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'databinding', pathMatch: 'full' },
   { path: 'databinding', component: DataBindingComponent },
     { path: 'directives', component: DirectivesComponent },
-    { path: 'student-list', component: StudentListComponent }
-
+    { path: 'student-list', component: StudentListComponent },
+      { path: 'about', component: AboutComponent },
+  { path: 'students', component: StudentListComponent },
+   { path: 'home', component: HomeComponent },
+   { path: '', component: HomeComponent },
 ];
