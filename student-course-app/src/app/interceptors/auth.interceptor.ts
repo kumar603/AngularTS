ac11c8cdbf11f95eacbcc67bd99098d443151089
Kiran Kumar <kumar603>
2025-08-06 12:21:56 +0530

    Angular TS - Environment Configurations - Dev and Production

--- student-course-app/src/app/interceptors/auth.interceptor.ts
+++ student-course-app/src/app/interceptors/auth.interceptor.ts
@@ -40,7 +40,7 @@ JIRA ID      : https://kiranjuvvanapudi.atlassian.net/browse/ATDAR-16
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
